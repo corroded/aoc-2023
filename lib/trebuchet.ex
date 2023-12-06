187c37c39d7961ffdb97f2681d116f8f9171726a
Eumir Gaspar <corroded@users.noreply.github.com>
2023-12-06 17:27:01 +0800

    Passing second test - but aoc says wrong answer

--- lib/trebuchet.ex
+++ lib/trebuchet.ex
@@ -1,14 +1,37 @@
 defmodule Trebuchet do
+  @word_to_num_mapping %{
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+  }
   def calibrate(value) do
-    String.split(value, "\n", trim: true)
-      |> Enum.map(fn line ->
-        rep = String.replace(line, ~r/[[:alpha:][:blank:]]+/, "")
-        "#{String.first(rep)}#{String.last(rep)}"
-      end)
-      |> Enum.map(fn num ->
-        {x, ""} = Integer.parse(num)
-        x
-      end)
-      |> Enum.sum
+    number_words = Map.keys(@word_to_num_mapping) |> Enum.map(&(to_string(&1)))
+    IO.inspect(number_words, label: "NW")
+    String.replace(value, number_words, fn mat -> words_to_num(mat) end)
+    |> String.split("\n", trim: true)
+    |> IO.inspect(label: "after split")
+    |> Enum.map(fn line ->
+      rep = String.replace(line, ~r/[[:alpha:][:blank:]]+/, "")
+      kek = "#{String.first(rep)}#{String.last(rep)}"
+      IO.inspect("converting line: #{line} to #{kek}")
+      kek
+    end)
+    |> Enum.map(fn num ->
+      {x, ""} = Integer.parse(num)
+      x
+    end)
+    |> Enum.sum
+  end
+
+  def words_to_num(value) do
+    {:ok, num} = Map.fetch(@word_to_num_mapping, value |> String.to_atom())
+    IO.inspect("value #{value} replaced to #{num}")
+    Integer.to_string(num)
   end
 end
--- test/trebuchet_test.exs
+++ test/trebuchet_test.exs
@@ -1,6 +1,5 @@
 defmodule TrebuchetTest do
   use ExUnit.Case
-  doctest AOC
 
   test "calculates calibration values" do
     values = """
@@ -10,5 +9,18 @@ defmodule TrebuchetTest do
       treb7uchet
     """
     assert Trebuchet.calibrate(values) == 142
+  end
+
+  test "takes into account number words" do
+    values = """
+      two1nine
+      eightwothree
+      abcone2threexyz
+      xtwone3four
+      4nineeightseven2
+      zoneight234
+      7pqrstsixteen
+    """
+    assert Trebuchet.calibrate(values) == 281
   end
 end
