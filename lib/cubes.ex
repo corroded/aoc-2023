0a5fd449f5bb797231c2a781f4a8c74b7ab80ad7
Eumir Gaspar <corroded@users.noreply.github.com>
2023-12-07 16:26:09 +0800

    Passing valid_game checker

--- lib/cubes.ex
+++ lib/cubes.ex
@@ -1,18 +1,26 @@
 defmodule Cubes do
- # def valid_game?(games) do
- #   String.split(games, ";")
- #   |> Enum.each(fn game ->
- #     with {:ok, red_green} <- parse_blue(game) do
- #       IO.inspect game
- #     end
- #   end)
- # end
+  @red_max 12
+  @green_max 13
+  @blue_max 14
+
+  def valid_game?(games) do
+    String.split(games, ";")
+    |> Enum.all?(fn game ->
+      with {:ok, colors} <- parse_colors(game) do
+        (colors["red"] == nil or colors["red"] <= @red_max) and
+        (colors["green"] == nil or colors["green"] <= @green_max) and
+        (colors["blue"] == nil or colors["blue"] <= @blue_max)
+      end
+    end)
+  end
 
   def parse_colors(game) do
-    Regex.scan(~r/([\d]+) ([red|blue|green]+)/, game)
+    colors = Regex.scan(~r/([\d]+) ([red|blue|green]+)/, game)
     |> Enum.reduce(%{}, fn x, acc ->
       [_, num, color] = x
       Map.put(acc, color, String.to_integer(num))
     end)
+
+    {:ok, colors}
   end
 end
